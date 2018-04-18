e1d5ebe59c1669bf478c0d2c5427464ea9acb415
Fabian Dario Montoya <nachofdm@hotmail.com>
2018-04-17 21:10:59 -0500

    Initial commit

---
+++ ReconocerTexto/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace ReconocerTexto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.txtTextoImagen = new System.Windows.Forms.TextBox();
+            this.btnCargarImagen = new System.Windows.Forms.Button();
+            this.FileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox.Location = new System.Drawing.Point(12, 55);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(378, 347);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // txtTextoImagen
+            // 
+            this.txtTextoImagen.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTextoImagen.Location = new System.Drawing.Point(425, 55);
+            this.txtTextoImagen.Multiline = true;
+            this.txtTextoImagen.Name = "txtTextoImagen";
+            this.txtTextoImagen.ReadOnly = true;
+            this.txtTextoImagen.Size = new System.Drawing.Size(399, 347);
+            this.txtTextoImagen.TabIndex = 1;
+            // 
+            // btnCargarImagen
+            // 
+            this.btnCargarImagen.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnCargarImagen.Location = new System.Drawing.Point(326, 420);
+            this.btnCargarImagen.Name = "btnCargarImagen";
+            this.btnCargarImagen.Size = new System.Drawing.Size(158, 40);
+            this.btnCargarImagen.TabIndex = 2;
+            this.btnCargarImagen.Text = "Seleccionar Imagen";
+            this.btnCargarImagen.UseVisualStyleBackColor = true;
+            this.btnCargarImagen.Click += new System.EventHandler(this.btnCargarImagen_Click);
+            // 
+            // FileDialog
+            // 
+            this.FileDialog.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(575, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Texto Capturado";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(836, 472);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCargarImagen);
+            this.Controls.Add(this.txtTextoImagen);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.TextBox txtTextoImagen;
+        private System.Windows.Forms.Button btnCargarImagen;
+        private System.Windows.Forms.OpenFileDialog FileDialog;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ReconocerTexto/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tesseract;
+using IronOcr;
+
+namespace ReconocerTexto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCargarImagen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Title = "Seleccione una imagen";
+            dialog.Filter = "Imágenes JPG (*.jpg;*.jpeg)|*.jpeg;*.jpg|Imágenes PNG (*.png)|*.png";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string fileName = dialog.FileName;
+                    Bitmap image = new Bitmap(fileName);
+                    pictureBox.Image = (System.Drawing.Image)image;
+                    string imageName = dialog.SafeFileName;
+
+                    //string path = Path.Combine(Environment.CurrentDirectory, "tessdata");
+                    //var imagen = new Bitmap(fileName);
+                    //var teseract = new TesseractEngine(path, "eng", EngineMode.Default);
+                    //var page = teseract.Process(imagen);
+                    //string text = page.GetText();
+                    //txtTextoImagen.Text = text;
+
+
+                    //=================================================================================
+                    //=================================================================================
+                    //using (var engine = new TesseractEngine(@"tessdata", "spa", EngineMode.Default))
+                    //{
+                    //    using (var img = Pix.LoadFromFile(fileName))
+                    //    {
+
+                    //        using (var page2 = engine.Process(img))
+                    //        {
+                    //            Console.WriteLine("GetMeanConfidence:\n" + page2.GetMeanConfidence());
+                    //            string text2 = page2.GetText();
+                    //            Console.WriteLine("Text2:\n" + text2);
+                    //            txtTextoImagen.Text = text2;
+                    //        }
+                    //    }
+                    //}
+                    //var Ocr = new AutoOcr();
+                    //var Result = Ocr.Read(fileName);
+                    //Console.WriteLine(Result.Text);
+                    //txtTextoImagen.Text = Result.Text;
+
+
+                    //=================================================================================
+                    //=================================================================================
+                    var Ocr = new AdvancedOcr()
+                    {
+                        CleanBackgroundNoise = true,
+                        EnhanceContrast = true,
+                        EnhanceResolution = true,
+                        Language = IronOcr.Languages.Spanish.OcrLanguagePack,
+                        Strategy = IronOcr.AdvancedOcr.OcrStrategy.Advanced,
+                        ColorSpace = AdvancedOcr.OcrColorSpace.Color,
+                        DetectWhiteTextOnDarkBackgrounds = true,
+                        InputImageType = AdvancedOcr.InputTypes.AutoDetect,
+                        RotateAndStraighten = false,
+                        ReadBarCodes = false,
+                        ColorDepth = 6
+                    };
+
+                    var testImage = fileName;
+
+                    var Results = Ocr.Read(testImage);
+
+                    Console.WriteLine("==================================");
+                    Console.WriteLine(Results.Text);
+                    txtTextoImagen.Text = Results.Text;
+                    Console.WriteLine("==================================");
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lo sentimos, ocurrió un error al intentar procesar tu imagen: \n\n" + ex, "Error al procesar texto de imagen", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+        }
+    }
+}
